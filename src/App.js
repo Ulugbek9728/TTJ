6089aabdf26d1f414a84bc2c0f39accd5a7224d9
ulugbek9728 <ulugbekshukurov9728@gmail.com>
2023-02-21 15:12:26 +0500

    i18next

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import {useTranslation} from "react-i18next";
+
+
 
 function App() {
+
+  const { t, i18n} = useTranslation();
+  const changeLanguage = (language)=>{
+    i18n.changeLanguage(language)
+  };
+
   return (
     <div className="App">
-      TTJ
+      <button className="btn btn-success" onClick={()=> changeLanguage("ru")}>RU</button>
+      <button className="btn btn-success" onClick={()=> changeLanguage("uz")}>UZ</button>
+
+      <span>{t("test")}</span>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./utils/i18n";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<span class="loader" />}>
+       <App />
+    </Suspense>
   </React.StrictMode>
 );
 
