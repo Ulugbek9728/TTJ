d187ec4e5c5de9cbab63b4cb33802f2bcf681f39
Sarvarbek <mukhtarovsarvarbek@gmail.com>
2023-06-03 00:14:54 +0500

    natija

--- src/pages/natija.js
+++ src/pages/natija.js
@@ -11,19 +11,43 @@ function Natija(props) {
     const [status, setStatus] = useState('');
     const [login, setLogin] = useState('');
     const [Student, setStudent] = useState({});
+    const [text, setText] = useState('');
+    const [fileUrl, setFileUrl] = useState('');
 
 
     function Login() {
         axios.post(`${ApiName1}/public/student/login`, {login: login}).then((response) => {
             console.log(response.data)
             setStudent(response.data)
-            setStatus(response.data.status)
+            setStatus(response.data.status);
+            switch (response.data?.dormitoryStudentStatus) {
+                case 'ACCEPTED': {
+                    setText('Tabriklaymiz quyidagi ruxsatnomani yuklab olishingiz mumkun.');
+                    setFileUrl(response.data?.response_file_url)
+                    break;
+                }
+                case 'REMOVED': {
+                    setText('Haydalgansiz');
+                    setFileUrl(response.data?.response_file_url)
+                    break;
+                }
+                case 'IS_ACCEPTED': {
 
+                    setText('Arizangiz ko\'rib chiqilmoqda');
+                    break;
+                }
+                case 'NOT_ACCEPTED': {
+                    setText('Arizangiz qabul qilinmadi!');
+                    break;
+                }
+            }
         }).catch((error) => {
             console.log(error)
         })
     }
 
+    console.log(text);
+    console.log(fileUrl);
     return (
         <>
             <Navbar/>
@@ -34,37 +58,24 @@ function Natija(props) {
                             <div className="title">
                                 Natija
                             </div>
-                            {status &&  <>
-                            {status === 'NOT_ACCEPTED' ?
-                                <div className="commit">
-                                    Sizning Arizangiz qabul qilinmadi,
-                                    to'liq ma'lumot uchun dekanatingizga uchrashing
-                                </div>
-                                :
-                                <>
-                                    {status === 'IS_ACCEPTED' ?
-                                        <div className="">
-                                    <span className='commit'>
-                                        Sizning arizangiz ko'rib chiqilmoqda!
-                                    </span>
-                                        </div> :
-                                        <div className="">
-                                    <span className='commit'>
-                                        Tabriklaymiz quyidagi ruxsatnomani yuklab olishingiz mumkun.
-                                    </span>
-                                            <br/>
-                                            <a href={`${ApiName1}${Student.response_file_url}`} target='_blank'>
-                                                Ruxsatnoma
-                                            </a>
-                                        </div>
-                                    }
-                                </>
-                            }
-                            </> }
-                            <>
-
-                            </>
-
+                            <div className="">
+                    <span className='commit'>
+                        {text}
+                    </span>
+                                <br/>
+                                {fileUrl && <a href={`${ApiName1}${fileUrl}`} target='_blank'>
+                                    file
+                                </a>}
+                            </div>
+                            {/*<div className="">*/}
+                            {/*    <span className='commit'>*/}
+                            {/*            Tabriklaymiz quyidagi ruxsatnomani yuklab olishingiz mumkun.*/}
+                            {/*    </span>*/}
+                            {/*    <br/>*/}
+                            {/*    <a href={`${ApiName1}${Student.response_file_url}`} target='_blank'>*/}
+                            {/*        Ruxsatnoma*/}
+                            {/*    </a>*/}
+                            {/*</div>*/}
                         </div>
 
                         <div className="right-side">
